12b2e03688e1e75f32f4a97825ebed7892f39e51
abrahamyada24 <abrahamyadaa24@gmail.com>
2026-06-19 06:25:20 +0700

    Update API_URL to use VPS IP

--- pos-android/src/services/api.ts
+++ pos-android/src/services/api.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // URL Backend - Lokal (10.0.2.2 adalah IP untuk Android Emulator mengakses localhost PC)
 // Jika menggunakan Device fisik (HP), ganti dengan IP IPv4 laptop Anda (contoh: http://192.168.1.10:5000)
-export const API_URL = 'http://10.0.2.2:5000'; 
+export const API_URL = 'http://103.175.221.2:5000'; 
 
 const api = axios.create({
     baseURL: `${API_URL}/api`,
