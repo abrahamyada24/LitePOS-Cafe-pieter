7b9d530e895a422666708480f90f1915f6859134
abrahamyada24 <abrahamyadaa24@gmail.com>
2026-06-19 08:07:55 +0700

    Add rewrites for VPS proxy

--- pos-fe/next.config.mjs
+++ pos-fe/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://103.175.221.2:5000/api/:path*'
+      },
+      {
+        source: '/uploads/:path*',
+        destination: 'http://103.175.221.2:5000/uploads/:path*'
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
